e93a93b9510a1067df4090cf8da171eab8b6862c
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-06-22 10:17:26 +0300

    initial

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 8, 'M', 'brown', 'playful', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 5, 'M', 'black', 'energetic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 6, 'F', 'gray', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 10, 'M', 'brown', 'serious', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 4, 'F', 'white', 'gentle', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 12, 'M', 'green', 'grumpy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 7, 'F', 'brown', 'friendly', 1);
+
+INSERT INTO bears (age, sex, color, temperament, alive)
+VALUES (3, 'M', 'brown', 'curious', 1);
